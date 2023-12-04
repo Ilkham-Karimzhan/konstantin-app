8b42387445d14ea5ed7f791f5798f512968255ae
Danil702 <danil_kim_2005@mail.ru>
2023-12-05 00:43:23 +0600

    add supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@vue-macros/nuxt', '@nuxtjs/tailwindcss', 'nuxt-svgo', '@vueuse/nuxt', 'vue3-carousel-nuxt', '@pinia/nuxt']
+  modules: ['@vue-macros/nuxt', '@nuxtjs/tailwindcss', 'nuxt-svgo', '@vueuse/nuxt', 'vue3-carousel-nuxt', '@pinia/nuxt', '@nuxtjs/supabase']
 })
